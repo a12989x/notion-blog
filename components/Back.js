4bf80462e586fbc3d1974245db6a52a91a4edee3
Alexis Guzman <alexis12989x@gmail.com>
2021-01-11 14:56:23 -0600

    Set up Button component

--- components/Back.js
+++ components/Back.js
@@ -1,8 +1,22 @@
-const Back = () => {
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Back = ({ cover = null }) => {
     return (
-        <div>
-            <p>Back Component</p>
-        </div>
+        <Link href="/">
+            <a className={`post__back ${cover ? 'post__back-cover' : ''}`}>
+                <button>
+                    <Image
+                        src="/assets/back.svg"
+                        alt="back"
+                        height="100%"
+                        width="100%"
+                        layout="responsive"
+                    />{' '}
+                    Go Back
+                </button>
+            </a>
+        </Link>
     );
 };
 
