7d0836c3cf9d6021a078a5675d1a98df4e3b356d
Alexis Guzman <alexis12989x@gmail.com>
2021-01-16 12:23:03 -0600

    Restructure paths mapping and add animations

--- pages/posts/[page].js
+++ pages/posts/[page].js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useContext } from 'react';
+import { motion } from 'framer-motion';
 
 import { RouterContext } from '../../context/RouterContext';
 
@@ -11,25 +12,27 @@ import {
     getPostsForPage,
     getFirstPost,
 } from '../../lib/api';
+import { fadeInUp } from '../../lib/animations';
 
 export const getStaticPaths = async () => {
     const posts = await getPublishedPosts();
     const postsOrderer = await orderPosts(posts);
-    const pagePosts = await getPostsForPage(5, postsOrderer, 5);
+    const pagePosts = await getPostsForPage(7, postsOrderer, 5);
+
+    const pagesNumber = pagePosts.map((post, index) => ++index);
 
     return {
-        paths: [],
-        // paths: pagePosts.map((post, index) => ({
-        //     params: { page: toString(++index) },
-        // })),
-        fallback: true,
+        paths: pagesNumber.map((pageNumber) => ({
+            params: { page: pageNumber.toString() },
+        })),
+        fallback: false,
     };
 };
 
 export const getStaticProps = async ({ params: { page } }) => {
     const posts = await getPublishedPosts();
     const postsOrderer = await orderPosts(posts);
-    const pagePosts = await getPostsForPage(5, postsOrderer, 5);
+    const pagePosts = await getPostsForPage(7, postsOrderer, 5);
 
     const firstPost = await getFirstPost(pagePosts[page - 1]);
 
@@ -50,7 +53,13 @@ const Posts = ({ posts, firstPost, pagesNumber }) => {
     if (router.isFallback) return <h1>Loading...</h1>;
 
     return (
-        <main className="posts">
+        <motion.main
+            className="posts"
+            variants={fadeInUp}
+            initial="initial"
+            animate={'animate'}
+            exit={{ opacity: 0 }}
+        >
             {posts.map((post) => (
                 <PostListItem
                     key={post.id}
@@ -80,7 +89,7 @@ const Posts = ({ posts, firstPost, pagesNumber }) => {
                     </Link>
                 ))}
             </section>
-        </main>
+        </motion.main>
     );
 };
 
