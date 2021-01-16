15f4067d67671b3f9b3d523e8e9aa04eeeed7f59
Alexis Guzman <alexis12989x@gmail.com>
2021-01-16 12:20:03 -0600

    Add animations

--- components/PostListItem.js
+++ components/PostListItem.js
@@ -1,15 +1,23 @@
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+import { scale, scaleTap } from '../lib/animations';
 
 const PostListItem = ({ slug, icon, title, description, date }) => {
     return (
         <Link href={`/post/${slug}`}>
-            <a className="postListItem">
+            <motion.a
+                className="postListItem"
+                whileHover={scale}
+                whileTap={scaleTap}
+                whileFocus={scale}
+            >
                 {icon ? `${icon} ${title}` : title}
                 {description ? (
                     <p className="postListItem__description">{description}</p>
                 ) : null}
                 <small className="postListItem__date">{date}</small>
-            </a>
+            </motion.a>
         </Link>
     );
 };
